895bcb0bf9bd782dba86bd28664a8d53db7cecd4
Maxumko <maxumkomaks111@gmail.com>
2017-04-22 17:58:09 +0300

    Add pins

--- git.ino
+++ git.ino
@@ -1,7 +1,9 @@
 void setup() {
   // put your setup code here, to run once:
-
-}
+  pinMode(5, OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
